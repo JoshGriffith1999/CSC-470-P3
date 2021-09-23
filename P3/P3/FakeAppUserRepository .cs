28f3d8c4dda684bb62c4081f30416bfa72b9d72f
Luke Christensen <Luke.a.christensen@gmail.com>
2021-09-22 22:27:37 -0500

    Filled out GetALL method. filled out GetByUserName method

--- P3/P3/FakeAppUserRepository .cs
+++ P3/P3/FakeAppUserRepository .cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Text;
 
 namespace P3
 {
     class FakeAppUserRepository : IAppUserRepository
     {
-
+        private static Dictionary<int, AppUser> AppUsers;
         //Methods from the IAppUserRepositry Interface
-        public bool Login(string UserName, string Password) { }
-        public List<AppUser> GetALL() { }
-        public void SetAuthentication(string UserName, bool IsAuthenticated) { }
-        public AppUser GetByUserName(string UserName) { }
+        public bool Login(string UserName, string Password) {
+          
+        }
+        public List<AppUser> GetALL() {
+            List<AppUser> users = new List<AppUser>();
+            foreach (KeyValuePair<int, AppUser> user in AppUsers)
+            {
+                users.Add(user.Value);
+            }
+            return users;
+        }
+        public void SetAuthentication(string UserName, bool IsAuthenticated) {
+            
+        }
+        public AppUser GetByUserName(string UserName) {
+            AppUser user;
+            bool ignore = AppUser.TryGetValue(UserName, out user);
+            return user;
+        }
     }
 }
