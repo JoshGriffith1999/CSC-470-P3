d87614bef347593e3899a464b1dd21158310585c
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-09-23 10:36:11 -0700

    Unit Tests Complete, Unit tests are targeted for .NET4.7.2 but P3 solution is .netcore3.1 , gives error when trying to find FakeAppUserRepository Class but P3 project is correctly referenced within the UnitTestFakeAppUserRepository.

---
+++ P3/UnitTestFakeAppUserRepository/Login.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestFakeAppUserRepository
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod]
+        public void LoginWithCorrectCredentialsSuccessful()
+        {
+            //Arrange
+            const string userName = "john";
+            const string password = "go";
+            FakeAppUserRepository EmpRepository = new FakeAppUserRepository();
+            //Act
+            bool correctCredentials = EmpRepository.Login(userName,password);
+            //Assert
+            Assert.IsTrue(correctCredentials);
+        }
+        public void LoginWithIncorrectCredentialsFail()
+        {
+            //Arrange
+            const string userName = "john";
+            const string password = "notgo";
+            FakeAppUserRepository EmpRepository = new FakeAppUserRepository();
+            //Act
+            bool incorrectCredentials = EmpRepository.Login(userName, password);
+            //Assert
+            Assert.IsFalse(incorrectCredentials);
+        }
+
+
+    }
+}
---
+++ P3/UnitTestFakeAppUserRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFakeAppUserRepository")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFakeAppUserRepository")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10ee7911-eab2-49cb-830b-7b6027aa69ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
