4a023bd0e1f409e9b97b7e1373bb4056eec860f2
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-11-16 19:23:20 -0800

    nb

---
+++ P3/P3/FormModifyIssue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P3
+{
+    public partial class FormModifyIssue : Form
+    {
+        FakeProjectRepository fakeProjectRepo = new FakeProjectRepository();
+        FakeIssueRepository fakeIssueRepo = new FakeIssueRepository();
+        List<Project> projects;
+        List<Issue> allIssues;
+        
+        public FormModifyIssue()
+        {
+            projects = fakeProjectRepo.GetAll();
+            curProjID = fakeProjectRepo.
+            allIssues = fakeIssueRepo.GetAll();
+            InitializeComponent();
+        }
+    }
+}
