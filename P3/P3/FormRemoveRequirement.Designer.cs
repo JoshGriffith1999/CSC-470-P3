0624802aa06396022c1761d97d8a9bb594667d00
Josh Griffith <45469632+JoshGriffith1999@users.noreply.github.com>
2021-11-30 21:18:52 -0600

    formremove
    formremove

--- P3/P3/FormRemoveRequirement.Designer.cs
+++ P3/P3/FormRemoveRequirement.Designer.cs
@@ -29,12 +29,93 @@ namespace P3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.featurelabel = new System.Windows.Forms.Label();
+            this.requirementLabel = new System.Windows.Forms.Label();
+            this.featureComboBox = new System.Windows.Forms.ComboBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // featurelabel
+            // 
+            this.featurelabel.AutoSize = true;
+            this.featurelabel.Location = new System.Drawing.Point(74, 43);
+            this.featurelabel.Name = "featurelabel";
+            this.featurelabel.Size = new System.Drawing.Size(43, 13);
+            this.featurelabel.TabIndex = 0;
+            this.featurelabel.Text = "Feature";
+            // 
+            // requirementLabel
+            // 
+            this.requirementLabel.AutoSize = true;
+            this.requirementLabel.Location = new System.Drawing.Point(77, 89);
+            this.requirementLabel.Name = "requirementLabel";
+            this.requirementLabel.Size = new System.Drawing.Size(72, 13);
+            this.requirementLabel.TabIndex = 1;
+            this.requirementLabel.Text = "Requirements";
+            // 
+            // featureComboBox
+            // 
+            this.featureComboBox.FormattingEnabled = true;
+            this.featureComboBox.Location = new System.Drawing.Point(152, 43);
+            this.featureComboBox.Name = "featureComboBox";
+            this.featureComboBox.Size = new System.Drawing.Size(474, 21);
+            this.featureComboBox.TabIndex = 2;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(551, 304);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(401, 304);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(155, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 155);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // FormRemoveRequirement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.featureComboBox);
+            this.Controls.Add(this.requirementLabel);
+            this.Controls.Add(this.featurelabel);
+            this.Name = "FormRemoveRequirement";
             this.Text = "FormRemoveRequirement";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label featurelabel;
+        private System.Windows.Forms.Label requirementLabel;
+        private System.Windows.Forms.ComboBox featureComboBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- P3/P3/FormRemoveRequirement.cs
+++ P3/P3/FormRemoveRequirement.cs
@@ -12,9 +12,32 @@ namespace P3
 {
     public partial class FormRemoveRequirement : Form
     {
+        FakeFeatureRepository Features = new FakeFeatureRepository();
+        FakeRequirementRepositpry Requirements = new FakeRequirementRepositpry();
+
+        List<Feature> features = new List<Feature>();
+        List<Requirement> requirements = new List<Requirement>();
         public FormRemoveRequirement()
         {
             InitializeComponent();
+        }
+
+        public FormRemoveRequirement(FakeFeatureRepository F, FakeRequirementRepositpry R, List<Feature> FL, List<Requirement> RL)
+        {
+            InitializeComponent();
+            CenterToScreen();
+            featureComboBox.Focus();
+
+            Features = F;
+            Requirements = R;
+            features = FL;
+            requirements = RL;
+
+            //Fill Features
+            foreach (Feature x in features) {
+
+                featureComboBox.Items.Add(x.Title);
+            }
         }
     }
 }
